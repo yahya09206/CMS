be2a66260d72af4c09706588d6cff3078ebd61f0
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-20 14:08:35 -0800

    fixed error where db wasnt connecting

--- include/db.php
+++ include/db.php
@@ -1,7 +1,22 @@
 <?php 
+//proper way to connect to db
+$db['db_host'] = 'localhost';
+$db['db_user'] = 'root';
+$db['db_pw'] = 'root';
+$db['db_name'] = 'cms';
+
+foreach($db as $key => $value) {
+	# code...
+	define(strtoupper($key), $value);
+}
+
 
 //connect to db
-$connection = mysqli_connect('localhost', 'root', 'root', 'cms');
+$connection = mysqli_connect(
+	DB_HOST, 
+	DB_USER, 
+	DB_PW, 
+	DB_NAME);
 
 if($connection){
 	echo "We are connected";
