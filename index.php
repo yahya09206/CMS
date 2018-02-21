86d83fdf9b997dc2ed6c8dd414fa7ffe0517ec29
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-21 14:19:55 -0800

    removed footer from index file

--- index.php
+++ index.php
@@ -171,26 +171,6 @@
 
         <hr>
 
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<?php 
+    include "include/footer.php"
+ ?>
