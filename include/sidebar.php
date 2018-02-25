80d84174df5e6c5d83a0aa6a23d8afdfcce736d9
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-24 22:06:25 -0800

    moved side widget to includes folder

--- include/sidebar.php
+++ include/sidebar.php
@@ -58,9 +58,6 @@
 </div>
 
 <!-- Side Widget Well -->
-<div class="well">
-    <h4>Side Widget Well</h4>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-</div>
+<?php include "widget.php" ?>
 
 </div>
