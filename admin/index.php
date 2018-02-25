661cf9f7ba12e69147305a9482ed974b05a93903
Yahya Hussein <yahyahussein91@gmail.com>
2018-02-25 15:12:03 -0800

    new categories file inside amin

--- admin/index.php
+++ admin/index.php
@@ -1,17 +1,11 @@
 <?php include "include/header.php" ?>
-
-
     <div id="wrapper">
         <?php include "include/nav.php" ?>
-
         <div id="page-wrapper">
-
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
-
 
                         <h1 class="page-header">
                             Welcome
@@ -20,11 +14,8 @@
                     </div>
                 </div>
                 <!-- /.row -->
-
             </div>
             <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
-
-    <?php include "include/footer.php" ?>
+    <!-- /#page-wrapper -->
+ <?php include "include/footer.php" ?>
