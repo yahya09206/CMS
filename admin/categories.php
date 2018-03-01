117a7d3b7a82a469a77e57f1cb0ae2e783122a50
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-01 10:41:22 -0800

    everything works after first refactor

--- admin/categories.php
+++ admin/categories.php
@@ -11,29 +11,7 @@
                             <small>Author Name</small>
                         </h1>
                         <div class="col-xs-6">
-                            <?php 
-                                 if (isset($_POST['submit'])) {
-                                     # code...
-                                    $cat_title = $_POST['cat_title'];
-
-                                    if($cat_title == "" || empty($cat_title)) {
-                                        # code...
-                                        echo "This field should not be empty";
-
-                                    }else{
-                                        $query = "INSERT INTO categories(cat_title) ";
-                                        $query .= "VALUE('{$cat_title}') ";
-
-                                        $create_category_query = mysqli_query($connection, $query);
-
-                                        if(!$create_category_query) {
-                                            # code...
-                                            die('Query Failed' . mysqli_error($connection));
-                                        }
-                                    }
-                                 }
-
-                            ?>
+                            <?php insert_categories(); ?>
                             <form action="" method="post">
                                 <div class="form-group">
                                     <label for="cat-title">Add Category </label>
--- admin/functions.php
+++ admin/functions.php
@@ -1,3 +1,26 @@
 <?php 
+function insert_categories(){
+	//global connection
+	global $connection;
+	if (isset($_POST['submit'])) {
+	     # code...
+	    $cat_title = $_POST['cat_title'];
 
+	    if($cat_title == "" || empty($cat_title)) {
+	        # code...
+	        echo "This field should not be empty";
+
+	    }else{
+	        $query = "INSERT INTO categories(cat_title) ";
+	        $query .= "VALUE('{$cat_title}') ";
+
+	        $create_category_query = mysqli_query($connection, $query);
+
+	        if(!$create_category_query) {
+	            # code...
+	            die('Query Failed' . mysqli_error($connection));
+	        }
+	    }
+	 }
+}
 ?>
