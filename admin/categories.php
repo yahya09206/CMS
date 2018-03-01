9f622c2614f3838aa9b77feb0c1a3e2d60444546
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-01 10:21:29 -0800

    moving update form to external page

--- admin/categories.php
+++ admin/categories.php
@@ -40,37 +40,6 @@
                                     <input type="text" class="form-control" name="cat_title">
                                     <input class="btn btn-primary" type="submit" name="submit" value="Add Category">
                                 </div>
-                            </form> 
-                            <form action="" method="post">
-                                <div class="form-group">
-                                    <label for="cat-title"> Edit Category </label>
-                                    <?php 
-                                        if(isset($_GET['edit'])) {
-                                            # code...
-                                            $the_cat_id = $_GET['edit'];
-                                        $query = "SELECT * FROM categories WHERE cat_id = $the_cat_id ";
-                                        $select_categories_id = mysqli_query($connection, $query);
-
-                                        while ($row = mysqli_fetch_assoc($select_categories_id)) {
-                                            # code...
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
-                                            ?>
-                                            <input value="<?php if(isset($cat_title)){echo $cat_title;} ?>" type="text" class="form-control" name="cat_title">
-                                        <?php }} ?>
-                                        <?php 
-                                        //UPDATE QUERY
-                                            if (isset($_POST['update_category'])) {
-                                                # code...
-                                                $the_cat_title = $_POST['cat_title'];
-                                                $query = "UPDATE categories SET cat_title = '{$the_cat_title}' WHERE cat_id = {$the_cat_id}";
-                                                $delete_query = mysqli_query($connection, $query);
-                                                //refresh page after clicking delete
-                                                header("Location: categories.php");
-                                            }
-                                        ?>
-                                    <input class="btn btn-primary" type="submit" name="update_category" value="Update Category">
-                                </div>
                             </form>    
                         </div>
                         <!-- Category Form -->
