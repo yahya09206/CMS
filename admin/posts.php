844a91d5229900266725fdff20fafe1c39e92edf
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-01 18:23:05 -0800

    finished adding fields to query

--- admin/posts.php
+++ admin/posts.php
@@ -32,10 +32,17 @@
                                         $query = "SELECT * FROM posts ";
                                         $select_posts = mysqli_query($connection, $query);
 
-                                        while ($row = mysqli_fetch_assoc($select_categories)) {
+                                        while ($row = mysqli_fetch_assoc($select_posts)) {
                                             # code...
-                                            $cat_id = $row['cat_id'];
-                                            $cat_title = $row['cat_title'];
+                                            $post_id = $row['post_id'];
+                                            $post_author = $row['post_author'];
+                                            $post_title = $row['post_title'];
+                                            $post_category_id = $row['post_category_id'];
+                                            $post_status = $row['post_status'];
+                                            $post_image = $row['post_image'];
+                                            $post_tags = $row['post_tags'];
+                                            $post_comment_count = $row['post_comment_count'];
+                                            $post_date = $row['post_date'];
                                         }
                                     ?>
                                     <td>10</td>
