b3df6fe0958a99e2f9112d151ca3fdab7e813704
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-02 10:26:44 -0800

    edit button is working

--- admin/include/edit_post.php
+++ admin/include/edit_post.php
@@ -1,3 +1,27 @@
+<!-- EDIT POSTS -->
+<?php 
+	if (isset($_GET['p_id'])) {
+		# code...
+		echo $_GET['p_id'];
+	}
+
+	$query = "SELECT * FROM posts ";
+    $select_posts = mysqli_query($connection, $query);
+
+    while ($row = mysqli_fetch_assoc($select_posts)) {
+        # code...
+        $post_id = $row['post_id'];
+        $post_author = $row['post_author'];
+        $post_title = $row['post_title'];
+        $post_category_id = $row['post_category_id'];
+        $post_status = $row['post_status'];
+        $post_image = $row['post_image'];
+        $post_tags = $row['post_tag'];
+        $post_comment_count = $row['post_comment_count'];
+        $post_date = $row['post_date'];
+    }
+?>
+
 <form action="" method="post" enctype="multipart/form-data">
 
 	<div class="form-group">
