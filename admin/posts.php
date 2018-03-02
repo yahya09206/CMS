7efc554794c48c7fe3f129dbcacb9d2e62664411
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-01 18:13:43 -0800

    created table with posts elements

---
+++ admin/posts.php
@@ -0,0 +1,36 @@
+<?php include "include/admin_header.php" ?>
+    <div id="wrapper">
+        <?php include "include/nav.php" ?>
+        <div id="page-wrapper">
+            <div class="container-fluid">
+                <!-- Page Heading -->
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h1 class="page-header">
+                            Welcome
+                            <small>Author Name</small>
+                        </h1>
+
+                        <table class="table table-bordered table-hover">
+                            <thead>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>Author</th>
+                                    <th>Title</th>
+                                    <th>Category</th>
+                                    <th>Status</th>
+                                    <th>Images</th>
+                                    <th>Tags</th>
+                                    <th>Comments</th>
+                                    <th>Date</th>
+                                </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </div>
+    <!-- /#page-wrapper -->
+<?php include "include/admin_footer.php" ?>
