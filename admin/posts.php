9fca27230cc38e1137042bf1f81ef50ea55b1d7e
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-01 18:35:18 -0800

    got pics to show up on posts table in admin

--- admin/posts.php
+++ admin/posts.php
@@ -51,7 +51,7 @@
                                             echo "<td>$post_title</td>";
                                             echo "<td>$post_category_id</td>";
                                             echo "<td>$post_status</td>";
-                                            echo "<td>$post_image</td>";
+                                            echo "<td><img width='100' src='../images/$post_image'></td>";
                                             echo "<td>$post_tags</td>";
                                             echo "<td>$post_comment_count</td>";
                                             echo "<td>$post_date</td>";
