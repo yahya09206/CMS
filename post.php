5a2e632b29b643d428e58c09aba5082ecca55342
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-03 11:47:36 -0800

    query to send to db

--- post.php
+++ post.php
@@ -60,9 +60,13 @@
                         $comment_author = $_POST['comment_author'];
                         $comment_email = $_POST['comment_email'];
                         $comment_content = $_POST['comment_content'];
-
+                        //Query to create comment and post
                         $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
                         $query .= "VALUES ($the_post_id, '{$comment_author}', {$comment_email}, {$comment_content}, 'unapproved', now())";
+
+                        //Query To send to datebase
+                        $create_comment_query = mysqli_query($connection, $query);
+                        confirm($create_comment_query);
                     }
 
                 ?>
