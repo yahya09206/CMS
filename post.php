9d748cba9c33385e569ea84f97e1ebe846c612b4
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-03 11:45:44 -0800

    delete white space

--- post.php
+++ post.php
@@ -63,7 +63,6 @@
 
                         $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
                         $query .= "VALUES ($the_post_id, '{$comment_author}', {$comment_email}, {$comment_content}, 'unapproved', now())";
-                    
                     }
 
                 ?>
