54877e92765da5ecb1a78b234b8fdae3de3b8a10
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-02 18:25:07 -0800

    adding show post page

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
 
                         while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
                             # code...
+                            $post_id = $row['post_id'];
                             $post_title = $row['post_title'];
                             $post_author = $row['post_author'];
                             $post_date = $row['post_date'];
@@ -30,7 +31,7 @@
 
                 <!-- First Blog Post -->
                 <h2>
-                    <a href="#"><?php echo $post_title ?></a>
+                    <a href="post.php?p_id=$post_id<?php echo $post_id; ?>"><?php echo $post_title ?></a>
                 </h2>
                 <p class="lead">
                     by <a href="index.php"><?php echo $post_author ?></a>
