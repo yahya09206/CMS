eb3209c4d9d829f648d7c59d3158d5207ea29750
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-04 13:06:19 -0800

    making final adjustments to comments

--- admin/include/add_posts.php
+++ admin/include/add_posts.php
@@ -18,7 +18,7 @@
 		// FUNCTION FOR IMAGES
 		move_uploaded_file($post_image_temp, "../images/$post_image");
 
-		$query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tag, post_comment_count, post_status) ";
+		$query = "INSERT INTO posts(post_category_id, post_title, post_author, post_date, post_image, post_content, post_tag, post_status) ";
 
 		$query .= "VALUES({$post_category_id},'{$post_title}','{$post_author}',now(),'{$post_image}','{$post_content}','{$post_tags}','{$post_status}' ) ";
 
