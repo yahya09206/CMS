d5c62a33c15760df0bfc03241bc466154fac6369
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-03 20:12:43 -0800

    whitespace

--- post.php
+++ post.php
@@ -64,7 +64,7 @@
                         $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
                         $query .= "VALUES ($the_post_id, '{$comment_author}', {$comment_email}, {$comment_content}, 'unapproved', now())";
 
-                        //Query To send to datebase
+                        //Query To send to datebase(COMPLETE MOST UPDATE VERSION)
                         $create_comment_query = mysqli_query($connection, $query);
                         if ($create_comment_query) {
                             # code...
