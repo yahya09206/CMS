4035a037eec86b1696656563591f5dac54f5eb82
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-04 17:48:11 -0800

    comments are acting up again

--- post.php
+++ post.php
@@ -62,7 +62,7 @@
                         $comment_content = $_POST['comment_content'];
                         //Query to create comment and post
                         $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-                        $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', '{$comment_content}', unapproved, now())";
+                        $query .= "VALUES ($the_post_id, '{$comment_author}', '{$comment_email}', {$comment_content}, unapproved, now())";
 
                         //Query To send to datebase(COMPLETE MOST UPDATE VERSION)
                         $create_comment_query = mysqli_query($connection, $query);
