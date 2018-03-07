8605a91c0a01f5bbb558b025a06f69edde6374c7
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 16:35:37 -0800

    made quick query to check

--- include/login.php
+++ include/login.php
@@ -1,11 +1,23 @@
 <?php include "db.php"; ?>
 
 <?php 
-	
+	// LOG USER INTO DB
 	if (isset($_POST['login'])) {
 		# code...
-		echo $_POST['username'];
-		echo $_POST['password'];
+		$username = $_POST['username'];
+		$password =  $_POST['password'];
+
+		//escape string prevent sql injection
+		$username = mysqli_real_escape_string($connection, $username);
+		$password = mysqli_real_escape_string($connection, $username);
+
+		// QUICK CHECK
+		$query = "SELECT * FROM users WHERE username = '{$username}' ";
+		$select_user_query = mysqli_query($connection, $query);
+
+		if(!$select_user_query){
+			die("QUERY FAILED" . mysqli_error($connection));
+		}
 	}
 
 
