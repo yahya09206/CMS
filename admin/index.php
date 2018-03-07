3e91d4d5d1fc5048d2ef7d1ab0c92e06885cc8ff
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 20:19:57 -0800

    added correct chart

--- admin/index.php
+++ admin/index.php
@@ -133,37 +133,31 @@
 
                 <div class="row">
                     <script type="text/javascript">
-                        google.charts.load("current", {packages:['corechart']});
-                        google.charts.setOnLoadCallback(drawChart);
-                        function drawChart() {
-                          var data = google.visualization.arrayToDataTable([
-                            ["Element", "Density", { role: "style" } ],
-                            ["Copper", 8.94, "#b87333"],
-                            ["Silver", 10.49, "silver"],
-                            ["Gold", 19.30, "gold"],
-                            ["Platinum", 21.45, "color: #e5e4e2"]
-                          ]);
+                      google.charts.load('current', {'packages':['bar']});
+                      google.charts.setOnLoadCallback(drawChart);
 
-                          var view = new google.visualization.DataView(data);
-                          view.setColumns([0, 1,
-                                           { calc: "stringify",
-                                             sourceColumn: 1,
-                                             type: "string",
-                                             role: "annotation" },
-                                           2]);
+                      function drawChart() {
+                        var data = google.visualization.arrayToDataTable([
+                          ['Year', 'Sales', 'Expenses', 'Profit'],
+                          ['2014', 1000, 400, 200],
+                          ['2015', 1170, 460, 250],
+                          ['2016', 660, 1120, 300],
+                          ['2017', 1030, 540, 350]
+                        ]);
 
-                          var options = {
-                            title: "Density of Precious Metals, in g/cm^3",
-                            width: 600,
-                            height: 400,
-                            bar: {groupWidth: "95%"},
-                            legend: { position: "none" },
-                          };
-                          var chart = new google.visualization.ColumnChart(document.getElementById("columnchart_values"));
-                          chart.draw(view, options);
+                        var options = {
+                          chart: {
+                            title: 'Company Performance',
+                            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+                          }
+                        };
+
+                        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+                        chart.draw(data, google.charts.Bar.convertOptions(options));
                       }
-                  </script>
-                  <div id="columnchart_values" style="width: 900px; height: 300px;"></div>
+                    </script>
+                    <div id="columnchart_values" style="width: 800px; height: 500px;"></div>
                 </div>
                 <!-- /.row -->
             </div>
