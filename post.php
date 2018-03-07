05c0b8bfe77c51e7c38312c64759b5f41616abc7
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 20:11:30 -0800

    fixed comment

--- post.php
+++ post.php
@@ -71,7 +71,7 @@
                             die('QUERY FAILED' . mysqli_error($connection));
                         }
 
-                        QUERY FOR COMMENT COUNT
+                        //QUERY FOR COMMENT COUNT
                         $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
                         $query .= "WHERE post_id = $the_post_id ";
                         $update_comment_count = mysqli_query($connection, $query);
