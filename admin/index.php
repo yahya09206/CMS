39d8b67c65930203bf17ccd51ec5191ae1cb4567
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 20:27:28 -0800

    done cleaning up chart

--- admin/index.php
+++ admin/index.php
@@ -138,11 +138,8 @@
 
                       function drawChart() {
                         var data = google.visualization.arrayToDataTable([
-                          ['Year', 'Sales', 'Expenses', 'Profit'],
-                          ['2014', 1000, 400, 200],
-                          ['2015', 1170, 460, 250],
-                          ['2016', 660, 1120, 300],
-                          ['2017', 1030, 540, 350]
+                          ['Data', 'Count'],
+                          ['Posts', 1000],
                         ]);
 
                         var options = {
