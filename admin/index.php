df84475bcd8fce3e2b2887423021a9acc64b8973
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 17:26:44 -0800

    session recognizes username

--- admin/index.php
+++ admin/index.php
@@ -9,7 +9,7 @@
                     <div class="col-lg-12">
                         <h1 class="page-header">
                             Welcome
-                            <small>Author Name</small>
+                            <small><?php echo $_SESSION['username']; ?></small>
                         </h1>
                     </div>
                 </div>
