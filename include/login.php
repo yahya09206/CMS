80935cf68ec61cbf904653ce1b2c63612b3a691f
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-06 17:51:46 -0800

    refactored session

--- include/login.php
+++ include/login.php
@@ -33,17 +33,15 @@
 
 		}
 		//ROUTE TO HOME PAGE IF INVALID LOGIN
-		if($username !== $db_username && $password !== $db_password ){
+		if($username === $db_username && $password === $db_password){
 			header("Location: ../index.php");
-			//REDIRECT TO ADMIN IF LOGIN INFO MATCH
-		}else if($username === $db_username && $password === $db_password) {
-			# code...
 			// SET NEW SESSION | ASSIGN FROM RIGHT TO LEFT
 			$_SESSION['username'] = $db_username;
 			$_SESSION['first_name'] = $db_fname;
 			$_SESSION['last_name'] = $db_lname;
 			$_SESSION['user_role'] = $db_role;
 			header("Location: ../admin");
+			//REDIRECT TO ADMIN IF LOGIN INFO MATCH
 		}else{
 			header("Location: ../index.php");
 		}
