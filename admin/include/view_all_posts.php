a7f9b99a1a0e1b46252ba5ae404d1c993431a698
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-09 17:38:22 -0800

    adding link to view post from admin page

--- admin/include/view_all_posts.php
+++ admin/include/view_all_posts.php
@@ -59,6 +59,7 @@
                 <th>Tags</th>
                 <th>Comments</th>
                 <th>Date</th>
+                <th>View</th>
                 <th>Edit</th>
                 <th>Delete</th>
             </tr>
@@ -108,6 +109,7 @@
                     echo "<td>$post_tags</td>";
                     echo "<td>$post_comment_count</td>";
                     echo "<td>$post_date</td>";
+                    echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a href='posts.php?delete={$post_id}'>Delete</a></td>";
                     echo "</tr>";
