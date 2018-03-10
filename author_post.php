374abc9966c2a0ece546e59d1bfd27447032ca45
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-10 11:22:20 -0800

    cleaned up author page

--- author_post.php
+++ author_post.php
@@ -37,7 +37,7 @@
                 <a href="#"><?php echo $post_title ?></a>
             </h2>
             <p class="lead">
-                by <a href="index.php"><?php echo $post_author ?></a>
+                All Posts By: <?php echo $post_author ?></a>
             </p>
             <p><span class="glyphicon glyphicon-time"></span> <?php echo $post_date ?></p>
             <hr>
