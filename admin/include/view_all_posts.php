52135016feef4be1c41e30a46734e794d0158aaa
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-10 12:28:06 -0800

    fixed rest views query

--- admin/include/view_all_posts.php
+++ admin/include/view_all_posts.php
@@ -139,7 +139,7 @@
                     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete? Cannot be undone'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
-                    echo "<td><a href='posts.php?reset={$post_id}''>Reset</a> {$post_views_count}</td>";
+                    echo "<td><a href='posts.php?reset={$post_id}'>{$post_views_count}</a></td>";
                     echo "</tr>";
                 }
             ?>
@@ -162,7 +162,7 @@ if (isset($_GET['reset'])) {
     # code...
     $the_post_id = $_GET['reset'];
 
-    $query = "UPDATE posts WHERE post_views = 0 WHERE post_id = {$the_post_id}";
+    $query = "UPDATE posts SET post_views = 0 WHERE post_id =" . mysqli_real_escape_string($connection, $_GET['reset']) . " ";
     $reset_query = mysqli_query($connection, $query);
     header("Location: posts.php");
 
