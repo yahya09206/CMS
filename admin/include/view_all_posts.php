176474dee4209398a5f4a4fa297ff1d96a38de99
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-09 17:17:14 -0800

    check all boxes finally works

--- admin/include/view_all_posts.php
+++ admin/include/view_all_posts.php
@@ -85,7 +85,7 @@
                     echo "<tr>";
                     ?>
                     <!-- ADD CHECK BOX TO FIELDS -->
-                    <td><input id='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id ?>'></td>
+                    <td><input class='checkBoxes' type='checkbox' name='checkBoxArray[]' value='<?php echo $post_id ?>'></td>
 
                     <?php
                     echo "<td>$post_id</td>";
--- admin/js/scripts.js
+++ admin/js/scripts.js
@@ -1,9 +1,21 @@
 $(document).ready(function(){
+	//SELECT ALL CHECKBOXES
+	$('#selectAllBoxes').click(function(event){
+		if (this.checked) {
+			$('.checkBoxes').each(function(){
+				this.checked = true;
+			});
+		}else{
+			$('.checkBoxes').each(function(){
+				this.checked = false;
+			});
+		}
+	});
 
 	//Ckeditor
-	ClassicEditor
-		.create( document.querySelector( '#body' ) )
-		.catch( error => {
-		    console.error( error );
-	});
+	// ClassicEditor
+	// 	.create( document.querySelector( '#body' ) )
+	// 	.catch( error => {
+	// 	    console.log(error);
+	// 	});
 });
