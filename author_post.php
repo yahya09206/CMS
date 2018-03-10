2bb57fbf190a979b4a59937484d44c440ef946b7
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-10 11:25:09 -0800

    more cleaning

--- author_post.php
+++ author_post.php
@@ -1,14 +1,10 @@
 <?php include "include/db.php" ?>
 <?php include "include/header.php" ?>
-
 <!-- Navigation -->
 <?php include "include/navigation.php" ?>
-
 <!-- Page Content -->
 <div class="container">
-
     <div class="row">
-
         <!-- Blog Entries Column -->
         <div class="col-md-8">
             <?php 
@@ -21,17 +17,14 @@
             //Display all posts by author
                 $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}' ";
                 $select_all_posts_query = mysqli_query($connection, $query);
-
-                    while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
-                        # code...
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_img = $row['post_image'];
-                        $post_content = $row['post_title'];
-
+                while ($row = mysqli_fetch_assoc($select_all_posts_query)) {
+                    # code...
+                    $post_title = $row['post_title'];
+                    $post_author = $row['post_author'];
+                    $post_date = $row['post_date'];
+                    $post_img = $row['post_image'];
+                    $post_content = $row['post_title'];
             ?>
-
             <!-- Author Posts -->
             <h2>
                 <a href="#"><?php echo $post_title ?></a>
@@ -46,50 +39,41 @@
             <p><?php echo $post_content ?></p>
             <hr>
             <?php } ?>
-
             <!-- Blog Comments -->
                 <!-- Catch comment date -->
-                <?php 
-                    if (isset($_POST['create_comment'])) {
+            <?php 
+                if (isset($_POST['create_comment'])) {
+                    # code...
+                        //CHECK IF FIELDS ARE EMPTY BEFORE SUBMITTING
+                        $the_post_id = $_GET['p_id'];
+                        $comment_author = $_POST['comment_author'];
+                        $comment_email = $_POST['comment_email'];
+                        $comment_content = $_POST['comment_content'];
+                    if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
                         # code...
-                            //CHECK IF FIELDS ARE EMPTY BEFORE SUBMITTING
-                            $the_post_id = $_GET['p_id'];
-                            $comment_author = $_POST['comment_author'];
-                            $comment_email = $_POST['comment_email'];
-                            $comment_content = $_POST['comment_content'];
-                        if (!empty($comment_author) && !empty($comment_email) && !empty($comment_content)) {
+                        //Query to create comment and post
+                        $query = "INSERT INTO comments(comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
+                        $query .= "VALUES($the_post_id,'{$comment_author}','{$comment_email}','{$comment_content}','unapproved',now())";
+                        //Query To send to datebase(COMPLETE MOST UPDATE VERSION)
+                        $create_comment_query = mysqli_query($connection, $query);
+                        if (!$create_comment_query) {
                             # code...
-                            //Query to create comment and post
-                            $query = "INSERT INTO comments(comment_post_id, comment_author, comment_email, comment_content, comment_status, comment_date)";
-                            $query .= "VALUES($the_post_id,'{$comment_author}','{$comment_email}','{$comment_content}','unapproved',now())";
-
-                            //Query To send to datebase(COMPLETE MOST UPDATE VERSION)
-                            $create_comment_query = mysqli_query($connection, $query);
-                            if (!$create_comment_query) {
-                                # code...
-                                die('QUERY FAILED' . mysqli_error($connection));
-                            }
-
-                            //QUERY FOR COMMENT COUNT
-                            $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
-                            $query .= "WHERE post_id = $the_post_id ";
-                            $update_comment_count = mysqli_query($connection, $query);
-                        }else{
-                            echo "<script>alert('PLEASE FILL OUT FORM COMPLETELY');</script>";
+                            die('QUERY FAILED' . mysqli_error($connection));
                         }
-                        //end of check
-
+                        //QUERY FOR COMMENT COUNT
+                        $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 ";
+                        $query .= "WHERE post_id = $the_post_id ";
+                        $update_comment_count = mysqli_query($connection, $query);
+                    }else{
+                        echo "<script>alert('PLEASE FILL OUT FORM COMPLETELY');</script>";
                     }
-
-                ?>
+                    //end of check
+                }
+            ?>
         </div>
-
         <!-- Blog Sidebar Widgets Column -->
         <?php include "include/sidebar.php" ?>
-
     </div>
     <!-- /.row -->
-
     <hr>
-
 <?php include "include/footer.php" ?>
