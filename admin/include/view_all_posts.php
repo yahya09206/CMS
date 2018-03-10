7051d611722b355c72992540794851a42c18c14e
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-10 12:22:17 -0800

    reset views query

--- admin/include/view_all_posts.php
+++ admin/include/view_all_posts.php
@@ -139,7 +139,7 @@
                     echo "<td><a href='../post.php?p_id={$post_id}'>View Post</a></td>";
                     echo "<td><a href='posts.php?source=edit_post&p_id={$post_id}'>Edit</a></td>";
                     echo "<td><a onClick=\"javascript: return confirm('Are you sure you want to delete? Cannot be undone'); \" href='posts.php?delete={$post_id}'>Delete</a></td>";
-                    echo "<td>{$post_views_count}</td>";
+                    echo "<td><a href='posts.php?reset={$post_id}''>Reset</a> {$post_views_count}</td>";
                     echo "</tr>";
                 }
             ?>
@@ -154,6 +154,16 @@ if (isset($_GET['delete'])) {
 
     $query = "DELETE FROM posts WHERE post_id = {$the_post_id}";
     $delete_query = mysqli_query($connection, $query);
+    header("Location: posts.php");
+
+}
+//RESET
+if (isset($_GET['reset'])) {
+    # code...
+    $the_post_id = $_GET['reset'];
+
+    $query = "UPDATE posts WHERE post_views = 0 WHERE post_id = {$the_post_id}";
+    $reset_query = mysqli_query($connection, $query);
     header("Location: posts.php");
 
 }
