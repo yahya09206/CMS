728491631bb2d86261572a9d05153aa480ab1a0e
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-10 11:20:45 -0800

    delete comment section when cliking on author

--- author_post.php
+++ author_post.php
@@ -82,57 +82,6 @@
                     }
 
                 ?>
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form action="" method="post" role="form">
-                        <div class="form-group">
-                            <label>Author</label>
-                            <input type="text" class="form-control" name="comment_author">
-                        </div>
-                        <div class="form-group">
-                            <label>Email</label>
-                            <input type="email" class="form-control" name="comment_email">
-                        </div>
-                        <div class="form-group">
-                            <label>Comment</label>
-                            <textarea name="comment_content" class="form-control" rows="3"></textarea>
-                        </div>
-                        <button type="submit" name="create_comment" class="btn btn-primary">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-                <!-- Query to select everything from comments -->
-                <?php 
-                    $query = "SELECT * FROM comments WHERE comment_post_id = {$the_post_id} ";
-                    $query .= "AND comment_status = 'approved' ";
-                    $query .= "ORDER BY comment_id DESC ";
-                    $select_comment_query = mysqli_query($connection, $query);
-                    if(!$select_comment_query){
-                        die("QUERY FAILED" . mysqli_error($connection));
-                    }
-                    while ($row = mysqli_fetch_array($select_comment_query)) {
-                        # code...
-                        $comment_date = $row['comment_date'];
-                        $comment_content = $row['comment_content'];
-                        $comment_author = $row['comment_author'];
-                ?>
-                        <!-- Comment -->
-                        <div class="media">
-                            <a class="pull-left" href="#">
-                                <img class="media-object" src="http://placehold.it/64x64" alt="">
-                            </a>
-                            <div class="media-body">
-                                <h4 class="media-heading"><?php echo $comment_author; ?>
-                                    <small><?php echo $comment_date; ?></small>
-                                </h4>
-                                <?php echo $comment_content; ?>
-                            </div>
-                        </div>
-                <?php } ?>
         </div>
 
         <!-- Blog Sidebar Widgets Column -->
