f53d68a879a25151c3f97c60cc591b48d9a7d9ea
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-14 20:10:21 -0700

    finished setting up user count query

--- admin/index.php
+++ admin/index.php
@@ -11,14 +11,19 @@
             $query = "SELECT * FROM users_online WHERE session = '$session' ";
             $send_query = mysqli_query($connection, $query);
             $count = mysqli_num_rows($send_query);
-            //check for count
+            //check for user if null then create new session
             if ($count == NULL) {
                 # code...
                 //insert into users_online table
-                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session, $time')";
+                mysqli_query($connection, "INSERT INTO users_online(session, time) VALUES('$session', '$time' ");
             }else {
-                
+                //if user already exists just update with new time
+                mysqli_query($connection, "UPDATE users_online SET time = '$time' WHERE session = '$session' ");
+
             }
+
+                $users_online_query = mysqli_query($connection, "SELECT * FROM users_online WHERE time < '$time_out' ");
+                $count_user = mysqli_num_rows($users_online_query);
         ?>
         <!-- Navigation -->
         <?php include "include/nav.php" ?>
