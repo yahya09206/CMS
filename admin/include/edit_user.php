66cc391c8813d72031188a317f7a79c5edc6c0f6
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-16 20:57:53 -0700

    clean up

--- admin/include/edit_user.php
+++ admin/include/edit_user.php
@@ -114,7 +114,7 @@ if (isset($_GET['edit_user'])) {
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Update User">
     </div>
 
 </form>
