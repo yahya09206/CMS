c307495a0bff2c6d2b295621dcde7255c5ddbf55
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-25 19:36:04 -0700

    set up function to prevent sql injection

--- admin/functions.php
+++ admin/functions.php
@@ -87,6 +87,11 @@ function findAllCategories(){
     }
 }
 
+//function to prevent sql injection
+function escape($string){
+	mysqli_real_escape_string($connection, trim($string));
+}
+
 function deleteCategories(){
 	global $connection;
 	 //QUERY TO DELETE SELECTED CATEGORY
