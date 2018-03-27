ee6d34cf5c25ea7052620ca217ab3fca25561244
Yahya Hussein <yahyahussein91@gmail.com>
2018-03-26 17:50:01 -0700

    hopefully this works

--- database.php
+++ database.php
@@ -1,10 +1,33 @@
 <?php
-$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+//Get Heroku ClearDB connection information
+$cleardb_url      = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server   = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db       = substr($cleardb_url["path"],1);
 
-$server = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$db = substr($url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
 
-$conn = new mysqli($server, $username, $password, $db);
+$db['default'] = array(
+    'dsn'   => '',
+    'hostname' => 'us-cdbr-iron-east-05.cleardb.net',
+    'username' => 'bd67d2a9c1e4fd',
+    'password' => '7627b1b8',
+    'database' => 'heroku_d6bedf5fd349af4',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt' => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
 ?>
